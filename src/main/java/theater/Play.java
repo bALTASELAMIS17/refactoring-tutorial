4b9aa36d2774f8653344c5acf67091ba945a8584
shourya <shourya.h.vardhan@gmail.com>
2025-11-20 22:31:39 -0500

    Complete Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,24 @@
 package theater;
 
+/**
+ * Class representing a play.
+ * @null null tag hooray
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
